f3a3188b89a40b9c5ac79dda596a88641323b65b
Aikar <aikar@aikar.co>
2017-02-17 23:58:23 -0500

    misc improvements
    tried to see if i could hack around react-css-modules by mutating
    the object, but doesn't appear to do the trick. but this is still
    good changes

--- merge.js
+++ merge.js
@@ -1,17 +1,17 @@
 const cx = require('classnames/bind');
 const assign = require('object-assign');
-function merge() {
-    var styles = mergeStyles({}, Array.prototype.slice.apply(arguments));
-    styles.mergeStyles = function() {
-        return mergeStyles(assign({}, styles), Array.prototype.slice.apply(arguments));
-    };
+function merge(styles) {
+    styles = mergeStyles(styles);
+    Object.defineProperty(styles, 'mergeStyles', {
+        enumerable: false,
+        value: function() {
+            return mergeStyles(assign({}, styles), Array.prototype.slice.apply(arguments));
+        }
+    });
     return styles;
 }
 
 function mergeStyles(style, styles) {
-    if (!styles || !styles.length) {
-        return style;
-    }
     var keys = Object.keys(style);
     for (var idx in keys) {
         var key = keys[idx];
@@ -21,25 +21,27 @@ function mergeStyles(style, styles) {
         style[key] = style[key].split(/ /);
     }
 
-    for (var _len = styles.length, _key = 0; _key < _len; _key++) {
-        var sty = styles[_key];
-        if (!sty) {
-            continue;
-        }
-        var keys = Object.keys(sty);
-        for (var key in keys) {
-            key = keys[key];
-            var val = sty[key];
-            if (typeof val !== 'string') {
+    if (styles) {
+        for (var _len = styles.length, _key = 0; _key < _len; _key++) {
+            var sty = styles[_key];
+            if (!sty) {
                 continue;
             }
-            val = val.split(/ /);
-            if (style[key]) {
-                style[key] = style[key].concat(val);
-            } else {
-                style[key] = [key].concat(val);
-            }
+            var keys = Object.keys(sty);
+            for (var key in keys) {
+                key = keys[key];
+                var val = sty[key];
+                if (typeof val !== 'string') {
+                    continue;
+                }
+                val = val.split(/ /);
+                if (style[key]) {
+                    style[key] = style[key].concat(val);
+                } else {
+                    style[key] = [key].concat(val);
+                }
 
+            }
         }
     }
     var keys = Object.keys(style);
@@ -48,6 +50,9 @@ function mergeStyles(style, styles) {
         var key = keys[idx];
         if (!Array.isArray(style[key])) {
             continue;
+        }
+        if (style[key].indexOf(key) === -1) {
+            style[key].unshift(key);
         }
         style[key] = style[key].filter(onlyUnique).join(' ');
         cxStyle[key] = style[key];
