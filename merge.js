3445ea297b624132f0e4e40dca68a93fe628ec72
Aikar <aikar@aikar.co>
2017-02-18 12:38:54 -0500

    Clean up code and add importStyles (mutate self)

--- merge.js
+++ merge.js
@@ -1,29 +1,44 @@
+/**
+ * @type {Function}
+ */
 const cx = require('classnames/bind');
 const assign = require('object-assign');
 function merge(styles) {
     styles = mergeStyles(styles);
+    // Expose a mergeStyles that returns a new object with the new classes appended
     Object.defineProperty(styles, 'mergeStyles', {
         enumerable: false,
         value: function() {
             return mergeStyles(assign({}, styles), Array.prototype.slice.apply(arguments));
         }
     });
+    // Expose an importStyles that mutates THIS styles object and appends the new classes
+    Object.defineProperty(styles, 'importStyles', {
+        enumerable: false,
+        value: function() {
+            return mergeStyles(styles, Array.prototype.slice.apply(arguments));
+        }
+    });
     return styles;
 }
 
-function mergeStyles(style, styles) {
-    var keys = Object.keys(style);
-    for (var idx in keys) {
-        var key = keys[idx];
-        if (typeof style[key] !== 'string') {
-            continue;
+function mergeStyles(styles, newStyles) {
+    (function() {
+        // Convert strings into an array of class names on input style object
+        var keys = Object.keys(styles);
+        for (var idx in keys) {
+            var key = keys[idx];
+            if (typeof styles[key] !== 'string') {
+                continue;
+            }
+            styles[key] = styles[key].split(/ /);
         }
-        style[key] = style[key].split(/ /);
-    }
+    })();
 
-    if (styles) {
-        for (var _len = styles.length, _key = 0; _key < _len; _key++) {
-            var sty = styles[_key];
+    if (newStyles) {
+        // Iterate new styles, and append classes
+        for (var len = newStyles.length, idx = 0; idx < len; idx++) {
+            var sty = newStyles[idx];
             if (!sty) {
                 continue;
             }
@@ -35,28 +50,32 @@ function mergeStyles(style, styles) {
                     continue;
                 }
                 val = val.split(/ /);
-                if (style[key]) {
-                    style[key] = style[key].concat(val);
+                if (styles[key]) {
+                    styles[key] = styles[key].concat(val);
                 } else {
-                    style[key] = [key].concat(val);
+                    styles[key] = [key].concat(val);
                 }
 
             }
         }
     }
-    var keys = Object.keys(style);
-    const cxStyle = cx.bind(style);
-    for (var idx in keys) {
-        var key = keys[idx];
-        if (!Array.isArray(style[key])) {
-            continue;
+    const cxStyle = cx.bind(styles);
+    (function() {
+        // Convert arrays back to strings and bind it to the CX object too.
+        var keys = Object.keys(styles);
+        for (var idx in keys) {
+            var key = keys[idx];
+            if (!Array.isArray(styles[key])) {
+                continue;
+            }
+            // Ensure we have our base key in the class list. We may of received a style object that did not enforce this.
+            if (styles[key].indexOf(key) === -1) {
+                styles[key].unshift(key);
+            }
+            styles[key] = styles[key].filter(onlyUnique).join(' ');
+            cxStyle[key] = styles[key];
         }
-        if (style[key].indexOf(key) === -1) {
-            style[key].unshift(key);
-        }
-        style[key] = style[key].filter(onlyUnique).join(' ');
-        cxStyle[key] = style[key];
-    }
+    })();
 
     return cxStyle;
 }
